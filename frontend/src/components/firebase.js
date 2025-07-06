4a6bc007b866bfc35f477b498cbf8d01ae2edb58
Mariyam_CH <146091557+MariyamChauhdry@users.noreply.github.com>
2025-07-06 12:40:39 +0500

    Update firebase.js

--- frontend/src/components/firebase.js
+++ frontend/src/components/firebase.js
@@ -3,12 +3,12 @@ import { getAuth } from "firebase/auth";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyBxyvvdKgYgSJ5p9E5zIERpV0EMOwot7Pg",
-  authDomain: "facedetect-f064e.firebaseapp.com",
-  projectId: "facedetect-f064e",
-  storageBucket: "facedetect-f064e.firebasestorage.app",
-  messagingSenderId: "40586135061",
-  appId: "1:40586135061:web:c24b7c8c5b39bc1033701c"
+  apiKey: "",
+  authDomain: "",
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: ""
 };
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
